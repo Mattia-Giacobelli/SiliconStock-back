05b5736742d2e0bf3070f1a72a25c78be5fa60ce
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-01-09 11:37:58 +0100

    create route to get products by category

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,19 @@
 const connection = require("../data/db");
 
+// category index
+function categoryIndex(req, res) {
+  const slug = req.params.slug;
+
+  const sql = 'SELECT * FROM categories JOIN products ON categories.id = products.category_id WHERE categories.slug = ?'
+
+  connection.query(sql, [slug], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (results.length === 0)
+      return res.status(404).json({ error: "Products not found" });
+    res.json(results)
+  })
+}
+
 // show
 function show(req, res) {
   const slug = req.params.slug;
@@ -14,4 +28,4 @@ function show(req, res) {
   });
 }
 
-module.exports = { show };
+module.exports = { categoryIndex, show };
